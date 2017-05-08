01b17a5e050ae00d27cc13e885bcb46d4a423c23
Jan Dubois <jan.dubois@suse.com>
2017-05-08 12:45:44 -0700

    Update pipeline to match cf-suse-stacks changes

--- tasks/make-rootfs/run.sh
+++ tasks/make-rootfs/run.sh
@@ -13,12 +13,12 @@ src-ci/tasks/common/start-docker.sh
 make -C src
 
 versioned_stack_filename="out/${STACK_NAME}${SUFFIX}-$(cat version/number).tar.gz"
-mv src/opensusefs2.tar.gz "${versioned_stack_filename}"
+mv src/${STACK_NAME}.tar.gz "${versioned_stack_filename}"
 
 versioned_receipt_filename="out/${STACK_NAME}${SUFFIX}-$(cat version/number).receipt.txt"
 echo "Rootfs SHASUM: $(sha1sum "$versioned_stack_filename" | awk '{print $1}')" > "$versioned_receipt_filename"
 echo "" >> "$versioned_receipt_filename"
-cat src/cflinuxfs2/opensusefs2_zypper.out >> "$versioned_receipt_filename"
+cat src/cflinuxfs2/${STACK_NAME}_zypper.out >> "$versioned_receipt_filename"
 
 # No need to diff the rootfs shasum, that will always be different
 diff -u \
