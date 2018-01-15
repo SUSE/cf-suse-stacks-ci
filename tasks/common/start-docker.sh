74811ba3d1fa9498deafc2cafb706382ccce1431
Alex Harford <alex.harford@suse.com>
2018-01-15 10:28:15 -0800

    Handle special cgroup cases
    Some cgroups are mounted as combined dirs, and systemd is a unique case.

--- tasks/common/start-docker.sh
+++ tasks/common/start-docker.sh
@@ -12,7 +12,15 @@ mkdir -p /sys/fs/cgroup
 mountpoint -q /sys/fs/cgroup || \
   mount -t tmpfs -o uid=0,gid=0,mode=0755 cgroup /sys/fs/cgroup
 
-sed -e '1d;s/\([^\t]\)\t.*$/\1/' < /proc/cgroups | while IFS= read -r d; do
+# Setup special cgroups that can't be taken directly from /proc/cgroups
+mkdir -p /sys/fs/cgroup/cpu,cpuacct
+mount -n -t cgroup -o cpu,cpuacct cpu,cpuacct /sys/fs/cgroup/cpu,cpuacct
+mkdir -p /sys/fs/cgroup/net_cls,net_prio
+mount -n -t cgroup -o net_cls,net_prio net_cls,net_prio /sys/fs/cgroup/net_cls,net_prio
+mkdir -p /sys/fs/cgroup/systemd
+mount -t cgroup cgroup -o none,name=systemd /sys/fs/cgroup/systemd
+
+sed -e '1d;s/\([^\t]\)\t.*$/\1/' < /proc/cgroups | egrep -v ^cpu$\|^cpuacct$\|^net_cls$\|^net_prio$ | while IFS= read -r d; do
   mkdir -p "/sys/fs/cgroup/$d"
   mountpoint -q "/sys/fs/cgroup/$d" || \
     mount -n -t cgroup -o "$d" "$d" "/sys/fs/cgroup/$d" || \
